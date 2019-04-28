b4f80eb05c90b1120dc3480aaf4ef885563bd35f
Shantell Moore <moores1@hawkmail.newpaltz.edu>
2019-04-28 16:01:18 -0400

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Nav from './components/Nav';
+
+const Home = () => <h1>You are home </h1>
+const About = () => <h1>I teach best webcourse</h1>
+const Friends = () => (<ul>
+  <li>...</li>
+  <li>...</li>
+  </ul>)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Nav />
+      <Route path="/" component={Home}/>
+      <Route path="/About" component={About}/>
+      <Route path="/Friends" component={Friends}/>
+    </Router>
+    
     </div>
   );
 }
